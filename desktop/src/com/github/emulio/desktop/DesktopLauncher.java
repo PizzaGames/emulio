28a2a7ce09ba89ccb4dcb0e68ff862289f2fc037
Marcelo Frau <marcelo.frau@kaffa.com.br>
2017-05-18 11:40:58 -0300

    Adding initial libGDX project files

---
+++ desktop/src/com/github/emulio/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.emulio.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.emulio.Emulio;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Emulio(), config);
+	}
+}
