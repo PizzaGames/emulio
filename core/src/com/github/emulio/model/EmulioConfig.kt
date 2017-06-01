1afb0f0b11361484ab247bc7ed695b0de816092f
Marcelo Frau <marcelofrau@gmail.com>
2017-06-01 14:20:40 -0300

    adding controller listener
    Former-commit-id: 893ca3ff5994d452e4a1074115b7b233db3431b5

--- core/src/com/github/emulio/model/EmulioConfig.kt
+++ core/src/com/github/emulio/model/EmulioConfig.kt
@@ -1,8 +1,8 @@
 package com.github.emulio.model
 
-import com.badlogic.gdx.Input
 import com.badlogic.gdx.Input.Keys
-import com.badlogic.gdx.controllers.mappings.Xbox
+import com.badlogic.gdx.controllers.Controller
+import com.badlogic.gdx.utils.SharedLibraryLoader
 
 
 class EmulioConfig {
@@ -64,8 +64,8 @@ class EmulioConfig {
 			lTrigger = Xbox.L_TRIGGER
 			rTrigger = Xbox.R_TRIGGER
 
-			axisX = Xbox.L_STICK_HORIZONTAL_AXIS
-			axisY = Xbox.L_STICK_VERTICAL_AXIS
+			axisX = Xbox.L_STICK_VERTICAL_AXIS
+			axisY = Xbox.L_STICK_HORIZONTAL_AXIS
 		})
 		debug = true
 	}
@@ -117,5 +117,140 @@ class InputConfig {
 	var pageDown: Int = -1
 
 	var exit: Int = -1
+}
+
+
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ * http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Mappings for the Xbox series of controllers. Works only on desktop so far.
+
+ * See [this
+   * image](https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/360_controller.svg/450px-360_controller.svg.png) which describes each button and axes.
+
+ * All codes are for buttons expect the L_STICK_XXX, R_STICK_XXX, L_TRIGGER and R_TRIGGER codes, which are axes.
+
+ * @author badlogic
+ */
+object Xbox {
+	// Buttons
+	val A: Int
+	val B: Int
+	val X: Int
+	val Y: Int
+	val GUIDE: Int
+	val L_BUMPER: Int
+	val R_BUMPER: Int
+	val BACK: Int
+	val START: Int
+	val DPAD_UP: Int
+	val DPAD_DOWN: Int
+	val DPAD_LEFT: Int
+	val DPAD_RIGHT: Int
+	val L_STICK: Int
+	val R_STICK: Int
+
+	// Axes
+	/** left trigger, -1 if not pressed, 1 if pressed  */
+	val L_TRIGGER: Int
+	/** right trigger, -1 if not pressed, 1 if pressed  */
+	val R_TRIGGER: Int
+	/** left stick vertical axis, -1 if up, 1 if down  */
+	val L_STICK_VERTICAL_AXIS: Int
+	/** left stick horizontal axis, -1 if left, 1 if right  */
+	val L_STICK_HORIZONTAL_AXIS: Int
+	/** right stick vertical axis, -1 if up, 1 if down  */
+	val R_STICK_VERTICAL_AXIS: Int
+	/** right stick horizontal axis, -1 if left, 1 if right  */
+	val R_STICK_HORIZONTAL_AXIS: Int
+
+	init {
+		if (SharedLibraryLoader.isWindows || SharedLibraryLoader.isLinux) {
+			A = 0
+			B = 1
+			X = 2
+			Y = 3
+			GUIDE = 8
+			L_BUMPER = 4
+			R_BUMPER = 5
+			BACK = 6
+			START = 7
+			DPAD_UP = -1
+			DPAD_DOWN = -1
+			DPAD_LEFT = -1
+			DPAD_RIGHT = -1
+			L_TRIGGER = 2
+			R_TRIGGER = 5
+			L_STICK_VERTICAL_AXIS = 1
+			L_STICK_HORIZONTAL_AXIS = 0
+			L_STICK = 9
+			R_STICK_VERTICAL_AXIS = 4
+			R_STICK_HORIZONTAL_AXIS = 3
+			R_STICK = 10
+		} else if (SharedLibraryLoader.isMac) {
+			A = 11
+			B = 12
+			X = 13
+			Y = 14
+			GUIDE = 10
+			L_BUMPER = 8
+			R_BUMPER = 9
+			BACK = 5
+			START = 4
+			DPAD_UP = 0
+			DPAD_DOWN = 1
+			DPAD_LEFT = 2
+			DPAD_RIGHT = 3
+			L_TRIGGER = 0
+			R_TRIGGER = 1
+			L_STICK_VERTICAL_AXIS = 3
+			L_STICK_HORIZONTAL_AXIS = 2
+			L_STICK = -1
+			R_STICK_VERTICAL_AXIS = 5
+			R_STICK_HORIZONTAL_AXIS = 4
+			R_STICK = -1
+		} else {
+			A = -1
+			B = -1
+			X = -1
+			Y = -1
+			GUIDE = -1
+			L_BUMPER = -1
+			R_BUMPER = -1
+			L_TRIGGER = -1
+			R_TRIGGER = -1
+			BACK = -1
+			START = -1
+			DPAD_UP = -1
+			DPAD_DOWN = -1
+			DPAD_LEFT = -1
+			DPAD_RIGHT = -1
+			L_STICK_VERTICAL_AXIS = -1
+			L_STICK_HORIZONTAL_AXIS = -1
+			L_STICK = -1
+			R_STICK_VERTICAL_AXIS = -1
+			R_STICK_HORIZONTAL_AXIS = -1
+			R_STICK = -1
+		}
+	}
+
+	/** @return whether the [Controller] is an Xbox controller
+	 */
+	fun isXboxController(controller: Controller): Boolean {
+		return controller.name.contains("Xbox")
+	}
 }
 
--- core/src/com/github/emulio/ui/input/InputManager.kt
+++ core/src/com/github/emulio/ui/input/InputManager.kt
@@ -4,8 +4,8 @@ import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.Input
 import com.badlogic.gdx.controllers.Controller
 import com.badlogic.gdx.controllers.Controllers
-import com.badlogic.gdx.controllers.mappings.Xbox
 import com.github.emulio.model.EmulioConfig
+import com.github.emulio.model.Xbox
 import mu.KotlinLogging
 
 
@@ -25,83 +25,159 @@ class InputManager(val listener: InputListener, val config: EmulioConfig) {
 	}
 	
 	fun update(delta: Float) {
-//		updateControllers(delta)
+		updateControllers(delta)
 		updateKey(delta)
 	}
 
 	var joyAlreadyPressed = false
 	var joyPressedElapsed = 0.0f
-	
+
+	var ConfirmButton = false
+	var CancelButton = false
+	var FindButton = false
+	var OptionsButton = false
+	var SelectButton = false
+	var ExitButton = false
+	var UpButton = false
+	var DownButton = false
+	var LeftButton = false
+	var RightButton = false
+	var PageUpButton = false
+	var PageDownButton = false
+
+
 	fun updateController(controller: Controller, delta: Float) {
 		val gamepad = gamepadsCfg[controller.name] ?: return
+
 
 //		if (!anyJoyPressed(controller)) {
 //			joyAlreadyPressed = false
 //			joyPressedElapsed = 0.0f
 //			return
 //		}
-//
-//		joyPressedElapsed += delta
-//
-//		if (joyAlreadyPressed && joyPressedElapsed < 0.09f) {
-//			return
-//		}
-		
-		if (controller.getButton(gamepad.confirm)) {
-			listener.onConfirmButton()
-		} else if (controller.getButton(gamepad.cancel)) {
-			listener.onCancelButton()
-		} else if (controller.getButton(gamepad.find)) {
-			listener.onFindButton()
-		} else if (controller.getButton(gamepad.options)) {
-			listener.onOptionsButton()
-		} else if (controller.getButton(gamepad.select)) {
-			listener.onSelectButton()
-		} else if (controller.getButton(gamepad.exit)) {
-			listener.onExitButton()
+
+		joyPressedElapsed += delta
+
+		if (joyAlreadyPressed && joyPressedElapsed < 0.08f) {
+			return
 		}
 		
-		if (controller.getButton(gamepad.up)) {
-			listener.onUpButton(1f)
+		if (controller.getButton(gamepad.confirm)) {
+			if (!ConfirmButton) {
+				listener.onConfirmButton()
+			}
+			ConfirmButton = true
+		} else if (controller.getButton(gamepad.cancel)) {
+			if (!CancelButton) {
+				listener.onCancelButton()
+			}
+			CancelButton = true
+		} else if (controller.getButton(gamepad.find)) {
+			if (!FindButton) {
+				listener.onFindButton()
+			}
+			FindButton = true
+		} else if (controller.getButton(gamepad.options)) {
+			if (!OptionsButton) {
+				listener.onOptionsButton()
+			}
+			OptionsButton = true
+		} else if (controller.getButton(gamepad.select)) {
+			if (!SelectButton) {
+				listener.onSelectButton()
+			}
+			SelectButton = true
+		} else if (controller.getButton(gamepad.exit)) {
+			if (!ExitButton) {
+				listener.onExitButton()
+			}
+			ExitButton = true
+		} else if (controller.getButton(gamepad.up)) {
+			if (!UpButton) {
+				listener.onUpButton(1f)
+			}
+			UpButton = true
 		} else if (controller.getButton(gamepad.down)) {
-			listener.onDownButton(1f)
+			if (!DownButton) {
+				listener.onDownButton(1f)
+			}
+			DownButton = true
 		} else if (controller.getButton(gamepad.left)) {
-			listener.onLeftButton(1f)
+			if (!LeftButton) {
+				listener.onLeftButton(1f)
+			}
+			LeftButton = true
 		} else if (controller.getButton(gamepad.right)) {
-			listener.onRightButton(1f)
+			if (!RightButton) {
+				listener.onRightButton(1f)
+			}
+			RightButton = true
 		} else if (controller.getButton(gamepad.pageUp)) {
-			listener.onPageUpButton(1f)
+			if (!PageUpButton) {
+				listener.onPageUpButton(1f)
+			}
+			PageUpButton = true
 		} else if (controller.getButton(gamepad.pageDown)) {
-			listener.onPageDownButton(1f)
+			if (!PageDownButton) {
+				listener.onPageDownButton(1f)
+			}
+			PageDownButton = true
+		} else {
+			joyAlreadyPressed = false
+			joyPressedElapsed = 0.0f
+
+			ConfirmButton = false
+			CancelButton = false
+			FindButton = false
+			OptionsButton = false
+			SelectButton = false
+			ExitButton = false
+			UpButton = false
+			DownButton = false
+			LeftButton = false
+			RightButton = false
+			PageUpButton = false
+			PageDownButton = false
 		}
 		
 		val x = controller.getAxis(gamepad.axisX)
-		if (x > 0f) {
-			listener.onRightButton(calculateIntensity(x))
-		} else {
-			listener.onLeftButton(calculateIntensity(x))
+		if (isOutsideDeadzone(x)) {
+			if (x > 0) {
+				listener.onRightButton(calculateIntensity(x))
+			} else {
+				listener.onLeftButton(calculateIntensity(x))
+			}
 		}
-		
+
+
 		val y = controller.getAxis(gamepad.axisY)
-		if (y > 0f) {
-			listener.onUpButton(calculateIntensity(x))
-		} else {
-			listener.onDownButton(calculateIntensity(x))
+		if (isOutsideDeadzone(y)) {
+			if (y > 0f) {
+				listener.onUpButton(calculateIntensity(y))
+			} else {
+				listener.onDownButton(calculateIntensity(y))
+			}
 		}
-		
+
 		val lt = controller.getAxis(gamepad.lTrigger)
-		if (lt > 0f) {
-			listener.onPageUpButton(calculateIntensity(lt))
+		if (isOutsideDeadzone(lt)) {
+			if (lt > 0f) {
+				listener.onPageUpButton(calculateIntensity(lt))
+			}
 		}
 		val rt = controller.getAxis(gamepad.rTrigger)
-		if (rt > 0f) {
-			listener.onPageDownButton(calculateIntensity(lt))
+		if (isOutsideDeadzone(rt)) {
+			if (rt > 0f) {
+				listener.onPageDownButton(calculateIntensity(lt))
+			}
 		}
 
-//		joyAlreadyPressed = true
-//		joyPressedElapsed = 0.0f
+		joyAlreadyPressed = true
+		joyPressedElapsed = 0.0f
 
 	}
+
+	private fun isOutsideDeadzone(value: Float) = value > 0.15f || value < -0.15f
 
 	private fun anyJoyPressed(controller: Controller): Boolean {
 
