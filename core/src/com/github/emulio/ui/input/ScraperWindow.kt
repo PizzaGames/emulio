e0d56e39ed41c43a35c9c5e4a3caddc025cfce13
thalescesarp <thalescesarp@gmail.com>
2018-02-12 16:17:04 -0200

    Update scraper platform image when scrolling

--- core/src/com/github/emulio/ui/input/ScraperWindow.kt
+++ core/src/com/github/emulio/ui/input/ScraperWindow.kt
@@ -1,15 +1,24 @@
 package com.github.emulio.ui.input
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.files.FileHandle
+import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.badlogic.gdx.scenes.scene2d.ui.Skin
 import com.badlogic.gdx.scenes.scene2d.ui.Window
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable
+import com.badlogic.gdx.utils.Scaling
 import com.github.czyzby.lml.annotation.LmlActor
 import com.github.czyzby.lml.parser.impl.AbstractLmlView
 import com.github.czyzby.lml.vis.util.VisLml
 import com.github.emulio.model.Platform
+import com.github.emulio.model.theme.Theme
+import com.github.emulio.model.theme.View
+import com.github.emulio.model.theme.ViewImage
 import com.kotcrab.vis.ui.VisUI
 
 class ScraperWindow(stage: Stage, skin: Skin) {
@@ -36,8 +45,19 @@ class ScraperView(stage: Stage): AbstractLmlView(stage) {
         return  "scraperViewId"
     }
 
-    fun setPlatform(platform: Platform){
-        window.titleLabel.setText(platform.name)
+    fun setPlatform(platform: Platform, theme: Theme){
+
         platformImage.name = platform.platformName
+        platformImage.drawable = getDrawableFromPlatformTheme(platform, theme)
+        window.titleLabel.setText(platform.name)
+    }
+
+    private fun getDrawableFromPlatformTheme(platform: Platform, theme: Theme): Drawable {
+        val systemView = checkNotNull(theme.findView("system"), { "System tag of theme ${platform.platformName} not found. please check your theme files." })
+        val logo = systemView.findViewItem("logo")!! as ViewImage
+        val texture = Texture(FileHandle(logo.path), true)
+        texture.setFilter(Texture.TextureFilter.MipMap, Texture.TextureFilter.MipMap)
+
+        return TextureRegionDrawable(TextureRegion(texture))
     }
 }
